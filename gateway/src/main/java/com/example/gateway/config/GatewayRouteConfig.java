9efee8d111dadab367ab31c20d9599b8a1f1cc78
Laxman Paudel <laxmansharmapaudel@gmail.com>
2025-12-26 13:35:25 +0545

    Add CorsWebFilter

--- gateway/src/main/java/com/example/gateway/config/GatewayRouteConfig.java
+++ gateway/src/main/java/com/example/gateway/config/GatewayRouteConfig.java
@@ -3,7 +3,11 @@ package com.example.gateway.config;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.context.annotation.Configuration;
+import java.util.Collections;
 
 @Configuration
 public class GatewayRouteConfig {
@@ -25,6 +29,19 @@ public class GatewayRouteConfig {
                                                 .uri("lb://NOTIFICATION"))
                                 .build();
 
+        }
 
-    }
+        @Bean
+        public CorsWebFilter corsWebFilter() {
+                CorsConfiguration corsConfig = new CorsConfiguration();
+                corsConfig.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+                corsConfig.setMaxAge(3600L);
+                corsConfig.addAllowedMethod("*");
+                corsConfig.addAllowedHeader("*");
+
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", corsConfig);
+
+                return new CorsWebFilter(source);
+        }
 }
